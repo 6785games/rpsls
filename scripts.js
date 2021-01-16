57ee60a88d9eda3f7d57dee9d5dfd07f82a25155
Samuele Mattiuzzo <samumatt@gmail.com>
2021-01-16 23:23:15 +0000

    Progress

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 const playerOneColor = '#FF2D00',
     playerTwoColor = '#001BFF',
     circlesMap = {
-        'rock': '#path850',
-        'paper': '#path846',
-        'scissors': '#path848',
-        'lizard': '#path852',
-        'spock': '#path829'
+        'rock': 'path850',
+        'paper': 'path846',
+        'scissors': 'path848',
+        'lizard': 'path852',
+        'spock': 'path829'
     },
     arrowsMap = {
-        'rock,lizard': '#path4741',
-        'lizard,spock': '#path4741-6',
-        'spock,scissors': '#path4741-6-4',
-        'scissors,paper': '#path4741-6-4-0',
-        'paper,rock': '#path4741-6-4-0-1',
-        'paper,spock': '#rect4899',
-        'rock,scissors': '#rect4899-6',
-        'lizard,paper': '#rect4899-6-1',
-        'spock,rock': '#rect4899-6-1-8',
-        'scissors,lizard': '#rect4899-6-1-8-6'
+        'rock,lizard': 'path4741',
+        'lizard,spock': 'path4741-6',
+        'spock,scissors': 'path4741-6-4',
+        'scissors,paper': 'path4741-6-4-0',
+        'paper,rock': 'path4741-6-4-0-1',
+        'paper,spock': 'rect4899',
+        'rock,scissors': 'rect4899-6',
+        'lizard,paper': 'rect4899-6-1',
+        'spock,rock': 'rect4899-6-1-8',
+        'scissors,lizard': 'rect4899-6-1-8-6'
     },
     verbsMap = {
         'rock,lizard': 'crushes',
@@ -40,4 +40,42 @@ function rpsls() {
     for (var i = 0; i < checkboxes.length; i++) {
       array.push(checkboxes[i].value)
     }
+
+    var test1 = array.join(),
+        test2 = array.reverse().join(),
+        winnerKey = '',
+        winnerColor = '',
+        playerOneChoice = array[0],
+        playerTwoChoice = array[1];
+
+    // get player one choice
+    document.querySelector(".svgClass").getSVGDocument().getElementById(
+        circlesMap[playerOneChoice]
+    ).style.stroke = playerOneColor;
+
+    // get player two choice
+    document.querySelector(".svgClass").getSVGDocument().getElementById(
+        circlesMap[playerTwoChoice]
+    ).style.stroke = playerTwoColor;
+
+    // decide winner
+    if (test1 in arrowsMap) {
+        winnerKey = test1;
+        winnerColor = playerOneColor;
+    }
+    else{
+        if (test2 in arrowsMap) {
+            winnerKey = test2;
+            winnerColor = playerTwoColor;       
+        }
+    }
+
+    document.querySelector(".svgClass").getSVGDocument().getElementById(
+        arrowsMap[winnerKey]
+    ).style.stroke = winnerColor;
+
+    var txt = winnerKey.replace(",", " " + verbsMap[winnerKey] + " ");
+
+    document.getElementById('rpsls-text').style.color = winnerColor;
+    document.getElementById('rpsls-text').innerHTML = txt;
 };
