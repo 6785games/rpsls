78594563d369b2b14835683000a957533f93c02d
Samuele Mattiuzzo <samumatt@gmail.com>
2021-01-20 23:20:38 +0000

    Fixed svg url

--- src/Figures.js
+++ src/Figures.js
@@ -5,7 +5,7 @@ class Figures extends React.Component {
     render() {
         return(
             <SvgLoader className="svg-wrapper"
-                path="https://raw.githubusercontent.com/6785games/rpsls/pubnub-react/src/512px-rpsls.svg" >
+                path="https://raw.githubusercontent.com/6785games/rpsls/main/src/512px-rpsls.svg" >
                 {
                     (this.props.p1Choice && this.props.p2Choice) && 
                     <SvgProxy selector={this.props.p1Circle} stroke={this.props.p1Color} /> &&
